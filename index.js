620e9afcb7e1d92c9d8edf1700d3fb32587ec23b
yasakdogra <yasak.dogra@gmail.com>
2023-10-20 23:26:45 -0700

    add mongoose schema

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const cors = require('cors')
 require('dotenv').config()
 
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true }
+})
+const User = mongoose.model('User', userSchema)
+
+const exerciseSchema = new mongoose.Schema({
+  userid: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: true },
+})
+const Exercise = mongoose.model('Exercise', exerciseSchema)
+
 app.use(cors())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
